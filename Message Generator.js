bfa8137764259f7ff5e9a3dae757252449c5ed81
Kammeron H. <hairston42@gmail.com>
2024-04-19 16:10:35 -0600

    Added code that reads a .txt file for messages and generates a formated message.

--- Message Generator.js
+++ Message Generator.js
@@ -1,5 +1,16 @@
-// random message generator by Kammeron Hairston.
+// random message generator by Kammeron Hairston.;
 
+//reads the Fortune Cookie Sayings.txt file and makes an array
+const fs = require('fs');
+const text = fs.readFileSync('./Fortune Cookie Sayings.txt').toString('utf-8');
+let textByLine = text.split('\n');
+
+
+//strips the messages to be just the messages
+const messages = textByLine.map( val => val.replace(/\r$/, ''));
+
+
+//generates a random list of lucky numbers
 const luckyNumberGenerator = () => { // generates numbers for Lucky Numbers
     const luckyNumbers = [];
 
@@ -11,3 +22,23 @@ const luckyNumberGenerator = () => { // generates numbers for Lucky Numbers
 
     return luckyNumbers;
 }
+
+//picks a random message from the list of messages
+const messagePicker = (messages) => {
+    const ranPicker = Math.floor(Math.random() * messages.length);
+
+    return messages[ranPicker];
+}
+
+
+//generates a message that is formated for the user.
+const messageGenerator = () => {
+    const message = messagePicker(messages);
+    const luckNumber = luckyNumberGenerator();
+    console.log('Hello and welcome to Fortuntastic!');
+    console.log(`Your Fortune is: ${message}`);
+  
+    console.log(`Your lucky numbers are ${luckNumber}`);
+}
+
+messageGenerator();
